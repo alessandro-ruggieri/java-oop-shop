fbbc8a13fe88d5e2e270cbaa2826b8a9882598b5
Alessandro Ruggieri <alessandro.ruggieri95.ar@gmail.com>
2024-11-14 04:05:49 +0100

    Created "Main" and "Prodotto" classes
    Full functionality with both "Main" and "Prodotto" classes

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -3,8 +3,27 @@ package org.lessons.java.shop;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Prodotto product1 = new Prodotto();
+		product1.setCode();
+		product1.name = "Samsung Galaxy S24";
+		product1.description = "Smartphone Samsung di ultima generazione";
+		product1.price = 799.99;
+		product1.tax = 0.22;
+		
+		Prodotto product2 = new Prodotto();
+		product2.setCode();
+		product2.name = "Apple iPhone 16";
+		product2.description = "Smartphone Apple di ultima generazione";
+		product2.price = 979.99;
+		product2.tax = 0.22;
+		
+		System.out.println("Il primo prodotto è: " + product1.productIdentification());
+		System.out.println("Il prezzo del primo prodotto è: " + product1.getBasePrice() + "€");
+		System.out.println(String.format("Il prezzo ivato del primo prodotto è %.2f", product1.getTaxedPrice()));
+		
+		System.out.println("Il secondo prodotto è: " + product2.productIdentification());
+		System.out.println("Il prezzo del secondo prodotto è: " + product2.getBasePrice() + "€");
+		System.out.println(String.format("Il prezzo ivato del secondo prodotto è %.2f", product2.getTaxedPrice()));
 	}
 
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,10 +1,31 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	int productCode;
+	String name;
+	String description;
+	double price;
+	double tax = 0.22;
+	
+	void setCode() {
+		Random ran = new Random();
+		productCode = ran.nextInt(99999);
 	}
-
+	
+	double getBasePrice() {
+		return price;
+	}
+	
+	double getTaxedPrice() {
+		double taxAmount = price * tax;
+		double taxedPrice = price + taxAmount;
+		return taxedPrice;
+	}
+	
+	String productIdentification() {
+		return productCode + "-" + name;
+	}
 }
